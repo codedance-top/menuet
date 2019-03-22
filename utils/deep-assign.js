b73133f7073727bb8aef44ce1e69e17c96db73a0
jinhy <jinhy@livebridge.com.cn>
2019-03-22 15:08:22 +0800

    fix AJV $merge keyword default value issue

--- utils/deep-assign.js
+++ utils/deep-assign.js
@@ -23,11 +23,10 @@ const deepAssign = module.exports = (target, ... sources) => {
 
       const targetValueType = typeof(target[key]);
 
-      if (Array.isArray(target[key])
+      if (source[key] === null
+          || Array.isArray(target[key])
           || Array.isArray(source[key])
-          || !(targetValueType === 'object' && sourceValueType === 'object')
-          || source[key] === null
-          || typeof(source[key]) === 'undefined') {
+          || !(targetValueType === 'object' && sourceValueType === 'object')) {
         target[key] = source[key];
       } else {
 
